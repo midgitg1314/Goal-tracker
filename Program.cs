747b5a188e115bc85acc25f72b717d2ca9f9e7ab
Gage Smith <114100318+midgitg1314@users.noreply.github.com>
2024-02-23 13:07:22 -0700

    Added C# project

---
+++ Program.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+        
+        GoalManagement goals = new GoalManagement();
+
+
+        Console.Write("\n*** Welcome to the Eternal Quest Program ****\n");
+        
+        Console.Write($"\n*** You currently have {goals.GetTotalPoints()} points! ***\n");
+
+        MainMenu choice = new MainMenu();
+        GoalMenu goalChoice = new GoalMenu();
+
+        int action = 0;
+
+        while (action != 6)
+        {
+            action = choice.UserChoice();
+
+            switch (action)
+            {
+                case 1:
+                    //CreateNewGoal(goals, textInfo, goalChoice);
+                    int goalInput = 0;
+                    while (goalInput != 5)
+                    {
+                        goalInput = goalChoice.GetGoalChoice();
+                        switch (goalInput)
+                        {
+                            case 1:
+                                Console.WriteLine("What is the name of your goal? ");
+                                string name = Console.ReadLine();
+                                name = textInfo.ToTitleCase(name);
+                                Console.WriteLine("What is a short description of your goal? ");
+                                string description = Console.ReadLine();
+                                description = textInfo.ToTitleCase(description);
+                                Console.Write("What is the amount of points associated with this goal? ");
+                                int points = int.Parse(Console.ReadLine());
+                                SimpleGoal sGoal = new SimpleGoal("Simple Goal:", name, description, points);
+                                goals.AddGoal(sGoal);
+                                goalInput = 5;
+                                break;
+                            case 2:
+                                TryClearConsole();
+                                Console.WriteLine("What is the name of your goal? ");
+                                name = Console.ReadLine();
+                                name = textInfo.ToTitleCase(name);
+                                Console.WriteLine("What is a short description of your goal? ");
+                                description = Console.ReadLine();
+                                description = textInfo.ToTitleCase(description);
+                                Console.Write("What is the amount of points associated with this goal? ");
+                                points = int.Parse(Console.ReadLine());
+                                EternalGoal eGoal = new EternalGoal("Eternal Goal:", name, description, points);
+                                goals.AddGoal(eGoal);
+                                goalInput = 5;
+                                break;
+                            case 3:
+                                Console.WriteLine("What is the name of your goal? ");
+                                name = Console.ReadLine();
+                                name = textInfo.ToTitleCase(name);
+                                Console.WriteLine("What is a short description of your goal? ");
+                                description = Console.ReadLine();
+                                description = textInfo.ToTitleCase(description);
+                                Console.Write("What is the amount of points associated with this goal? ");
+                                points = int.Parse(Console.ReadLine());
+                                int numberTimes = int.Parse(Console.ReadLine());
+                                Console.Write("How many times does this goal need to be accomplished for a bonus? ");
+                                Console.Write("What is the bonus for accomplishing it that many times? ");
+                                int bonusPoints = int.Parse(Console.ReadLine());
+                                ChecklistGoal clGoal = new ChecklistGoal("Check List Goal:", name, description, points,numberTimes, bonusPoints);
+                                goals.AddGoal(clGoal);
+                                goalInput = 5;
+                                break;
+                            case 4:
+                                Console.WriteLine("What is the name of your goal? ");
+                                name = Console.ReadLine();
+                                name = textInfo.ToTitleCase(name);
+                                Console.WriteLine("What is a short description of your goal? ");
+                                description = Console.ReadLine();
+                                description = textInfo.ToTitleCase(description);
+                                Console.Write("What is the amount of points associated with this goal? ");
+                                points = int.Parse(Console.ReadLine());
+                                NegativeGoal nGoal = new NegativeGoal("Negative Goal:", name, description, points);
+                                goals.AddGoal(nGoal);
+                                goalInput = 5;
+                                break;
+                            case 5:
+                                break;
+                            case 6:
+                                Console.WriteLine($"\nThank you for using the Eternal Quest Program!\n");
+                                break;
+                            default:
+                                Console.WriteLine($"\nSorry the option you entered is not valid.");
+                                break;
+                        }
+                    }
+                    break;
+                case 2:
+                    TryClearConsole();
+                    Console.Write($"\n*** You currently have {goals.GetTotalPoints()} points! ***\n");
+                    goals.ListGoals();
+                    break;
+                case 3:
+                    goals.SaveGoals();
+                    break;
+                case 4:
+                    TryClearConsole();
+                    Console.Write($"\n*** You currently have {goals.GetTotalPoints()} points! ***\n");
+                    goals.LoadGoals();
+                    break;
+                case 5:
+                    TryClearConsole();
+                    Console.Write($"\n*** You currently have {goals.GetTotalPoints()} points! ***\n");
+                    goals.RecordGoalEvent();
+                    break;
+                case 6:
+                    Console.WriteLine("\nThank you for using the Eternal Quest Program!\n");
+                    break;
+                default:
+                    Console.WriteLine($"\nSorry the option you entered is not valid.");
+                    break;
+            }
+        }
+    }
+    static void TryClearConsole()
+    {
+        try
+        {
+            Console.Clear(); // Try to clear the console using the built-in method
+        }
+        catch (IOException)
+        {
+            // Handle IOException if Console.Clear() fails
+            ClearConsoleFallback();
+        }
+    }
+static void ClearConsoleFallback()
+    {
+        if (!Console.IsOutputRedirected)
+        {
+            // Simulate clearing by printing newlines
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < Console.WindowHeight; i++)
+            {
+                Console.WriteLine(new string(' ', Console.WindowWidth));
+            }
+            Console.SetCursorPosition(0, 0);
+        }
+    }
+}
